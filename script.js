0937c1f736fe5d43e9a250a7ee3c61bff2a3f0f5
NVision021 <nversic2@gmail.com>
2025-04-06 13:10:13 +1000

    Adjust checkWin function
    Function now checks for draw and takes a parameter of the player whose turn it is so that it is not required to check both players tokens unnecessarily

--- script.js
+++ script.js
@@ -75,7 +75,12 @@ const gameHandler = (function() {
     }
   }
 
-  const checkWin = () => {
+
+  //need to update this by 
+  //1) passing a parameter to check win with whose turn it is (then, when calling the function for a turn, store whose turn it is in a variable at the start of the turn and pass that to checkWin)
+  //2) deleting checking for both players: only one player needs to be checked (the player who just placed the token)
+  //3) add logic for a draw (else if number of tokens = 9, announce a draw)
+  const checkWin = (turnPlayer) => {
     let winningCombinations = [
       [0, 1, 2],
       [3, 4, 5], 
@@ -87,32 +92,32 @@ const gameHandler = (function() {
       [2, 4, 6],
     ]
 
-    for (let playerKey in gameHandler.getPlayers()) {
-      player = gameHandler.getPlayers()[playerKey];
-      let tokenPositions = [];
-      let count = 0;
-      for (let placedToken of gameboard.getBoard()) {
-        if (placedToken === player.getToken()){
-          tokenPositions.push(count);
-          count++;
-        }
+    let playerName = turnPlayer.getName();
+    let playerToken = turnPlayer.getToken();
+    let tokenPositions = [];
+    let count = 0;
+
+    for (let placedToken of gameboard.getBoard()) {
+      if (placedToken === playerToken){
+        tokenPositions.push(count);
+        count++;
       }
-      
-      for (let combination of winningCombinations) {
-        if (combination.every((value) => tokenPositions.includes(value))) {
-          console.log(`${player.getName()} wins!!`);
-          break;
-        }
+    }
+    
+    for (let combination of winningCombinations) {
+      if (combination.every((value) => tokenPositions.includes(value))) {
+        console.log(`${playerName} wins!!`);
+        break;
+      } else if (gameboard.getCounters() >= 9) {
+        console.log("It's a draw!!");
+        break;
       }
     }
   }
-  /*
+
+  //const takeTurn (should call a series of other functions from this function factory, may just be for playing in the console
+
   
-  const takeTurn (should call a series of other functions from this function factory, may just be for playing in the console
-
-
-*/
-
   return{getPlayers, startGame, getWhoseTurn, checkWin};
 })();
 
@@ -120,4 +125,4 @@ gameHandler.startGame(['Nick', 'x', true], ['Grace', 'o', false]);
 gameboard.addToken(0, 'x');
 gameboard.addToken(1, 'x');
 gameboard.addToken(2, 'x');
-gameHandler.checkWin();
+gameHandler.checkWin(createPlayer('Nick', 'x', true));
